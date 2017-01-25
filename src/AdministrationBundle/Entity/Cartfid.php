5dc899a5ecb663d6d9c642f34c49e26c0ebd393f
Dev Team <ilp@french-co.fr>
2017-01-25 11:44:53 +0100

    premier tableau fid

---
+++ src/AdministrationBundle/Entity/Cartfid.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace AdministrationBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Cartfid
+ *
+ * @ORM\Table(name="cartfid")
+ * @ORM\Entity(repositoryClass="AdministrationBundle\Repository\CartfidRepository")
+ */
+class Cartfid
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return Cartfid
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+}
